e75f19eb3e77c8707d4794e6bd059db140dd496e
SKJ811 <476212140@qq.com>
2018-08-15 11:56:52 +0800

    test1.0

---
+++ b/src/a/test.java
@@ -0,0 +1,11 @@
+package a;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		
+	}
+}
